c07cd531aec35f348365d37e1f06803fb58f6584
kevin sinza <sbsco3@SBSCO3s-MacBook-Pro.local>
2022-09-11 13:36:52 -0500

    create a project

---
+++ NotesApp/NotesApp WatchKit Extension/NotesAppApp.swift
@@ -0,0 +1,21 @@
+//
+//  NotesAppApp.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by SBSCO3 on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
---
+++ NotesApp/NotesApp WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by SBSCO3 on 11/09/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
